38982ac0130344932a61d3bdc56f0c7df5140f9d
Aziz <bemyslavedarlin@gmail.com>
2020-05-03 09:45:41 +0300

    fix sorry

--- app/src/Commands/CommandSorry.php
+++ app/src/Commands/CommandSorry.php
@@ -12,6 +12,6 @@ class CommandSorry extends Command
     public function run(Config $vkConfig, Database $database): void
     {
         $this->params['peer_id'] = $this->payload->getPeerId();
-        $this->params['message'] = 'Извените, йа сломалса.';
+        $this->params['message'] = 'Извените, йа сломалса. Daddy fixing me right now.. Espérez, みんな! &#129302;';
     }
 }
